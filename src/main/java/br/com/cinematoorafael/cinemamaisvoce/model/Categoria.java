bff475dc07a9f69112b98b245c72a1da4fbb4245
Rafinha <rafaelnarcisodasilva@gmail.com>
2026-02-23 21:28:29 -0300

    menu atulizado

--- src/main/java/br/com/cinematoorafael/cinemamaisvoce/model/Categoria.java
+++ src/main/java/br/com/cinematoorafael/cinemamaisvoce/model/Categoria.java
@@ -2,36 +2,37 @@ package br.com.cinematoorafael.cinemamaisvoce.model;
 
 public enum Categoria {
 
-    ACAO("Action"),
-    AVENTURA("Adventure"),
-    ANIMACAO("Animation"),
-    BIOGRAFIA("Biography"),
-    COMEDIA("Comedy"),
-    CRIME("Crime"),
-    DOCUMENTARIO("Documentary"),
-    DRAMA("Drama"),
-    FANTASIA("Fantasy"),
-    FICCAO_CIENTIFICA("Sci-Fi"),
-    GUERRA("War"),
-    HISTORIA("History"),
-    MISTERIO("Mystery"),
-    MUSICAL("Musical"),
-    ROMANCE("Romance"),
-    SUSPENSE("Thriller"),
-    TERROR("Horror"),
-    ESPORTE("Sport"),
-    FAMILIA("Family"),
-    FAROESTE("Western"),
-    POLICIAL("Crime"),
-    SUPER_HEROI("Superhero"),
-    THRILLER("Thriller"),
-    OUTRO("Unknown");
+    ACAO("Action", "Ação"),
+    AVENTURA("Adventure", "Aventura"),
+    ANIMACAO("Animation", "Animação"),
+    BIOGRAFIA("Biography", "Biografia"),
+    COMEDIA("Comedy", "Comédia"),
+    CRIME("Crime", "Crime"),
+    DOCUMENTARIO("Documentary", "Documentário"),
+    DRAMA("Drama", "Drama"),
+    FANTASIA("Fantasy", "Fantasia"),
+    FICCAO_CIENTIFICA("Sci-Fi", "Ficção Científica"),
+    GUERRA("War", "Guerra"),
+    HISTORIA("History", "História"),
+    MISTERIO("Mystery", "Mistério"),
+    MUSICAL("Musical", "Musical"),
+    ROMANCE("Romance", "Romance"),
+    SUSPENSE("Thriller", "Suspense"),
+    TERROR("Horror", "Terror"),
+    ESPORTE("Sport", "Esporte"),
+    FAMILIA("Family", "Família"),
+    FAROESTE("Western", "Faroeste"),
+    SUPER_HEROI("Superhero", "Super-herói"),
+    OUTRO("Unknown", "Outro");
 
+    private String categoriaPortugues;
 
     private String categoriaOmdb;
 
-    Categoria(String categoriaOmdb) {
+
+    Categoria(String categoriaOmdb, String categoriaPortugues) {
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaPortugues = categoriaPortugues;
     }
 
     public String getCategoriaOmdb() {
@@ -49,5 +50,20 @@ public enum Categoria {
             }
         }
         throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+
     }
+
+    public static Categoria fromPortugues(String text) {
+        if (text == null || text.isBlank()) {
+            return OUTRO;
+        }
+
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
 }
